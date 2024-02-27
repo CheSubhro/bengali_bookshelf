8b1a30881abb30a14a36fe328a1e14d4f2eda5f4
Subhro <92868017+CheSubhro@users.noreply.github.com>
2024-02-27 17:25:39 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
     <div className='mt-16 mx-10 md:mx-40 flex gap-28'>
         <div>
           <h1 className='text-7xl font-bold mb-10'>Unlock Worlds, <br />turn pages...</h1>
-          <p className='text-xl'>"Journey through the pages, find solace in words, and let the stories within shape your world."</p>
+          <p className='text-xl'>Journey through the pages, find solace in words, and let the stories within shape your world.</p>
         </div>
         <div className='w-72'>
         <Swiper
